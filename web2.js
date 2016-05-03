58d7d52fffced4b7f6ab39814b67be2987408682
joshftx <joshftx@gmail.com>
2016-05-03 07:39:07 -0700

    051315

--- web2.js
+++ web2.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var d3   = require('d3');
 var fs = require('fs');
+var Syslogd = require('syslogd')
+
 
 app.use(express.static('public'));
 
@@ -16,10 +18,9 @@ var fbdestination = [{origin: {latitude: 38.895111,longitude: -77.036667}, desti
                 longitude: 34.783333 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}}]
 
 
-
 io.on('connection', function(socket){
   console.log('a user connected');
- io.emit('message', {'message': fbdestination, for: 'everyone'});
+ socket.emit('message', {'message': fbdestination, for: 'everyone'});
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
@@ -27,13 +28,30 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000);
+var syslogParser = require('glossy').Parse; // or wherever your glossy libs are
+var dgram  = require("dgram");
+var server = dgram.createSocket("udp4");
 
-var file = fs.readFile('./data.csv', 'utf8', function (err, data) {
-  d3.csv.parse(data);
+server.on("message", function(rawMessage) {
+    syslogParser.parse(rawMessage.toString('utf8', 0), function(parsedMessage){
+        //console.log(parsedMessage.host + ' - ' + parsedMessage.message);
+        console.log(parsedMessage.src)
+        console.log(parsedMessage.host)
+    });
 });
 
-console.log (file);
+server.on("listening", function() {
+    var address = server.address();
+    console.log("Server now listening at " + 
+        address.address + ":" + address.port);
+});
+
+server.bind(514); // Remember ports < 1024 need suid
+
+
+http.listen(3000);
+
+
 
 
 
