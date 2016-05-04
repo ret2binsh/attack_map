e0764db28c4d4e9dfa8becbbf66b6b96f3948204
joshftx <joshftx@gmail.com>
2016-05-03 21:39:43 -0700

    working array

--- web2.js
+++ web2.js
@@ -13,14 +13,92 @@ app.use(express.static('public'));
 //var fbdestination = "destination: {latitude: 37.636519, longitude: -122.120506}"
 //var dests = new Array ();
 //console.log(fbdestination);
+//var fbdestination = [{origin: {latitude: 38.895111,longitude: -77.036667}, destination: {
+       //         latitude: 32.066667,
+         //       longitude: 34.783333 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}}]
+
+
+
 var fbdestination = [{origin: {latitude: 38.895111,longitude: -77.036667}, destination: {
-                latitude: 32.066667,
-                longitude: 34.783333 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}}]
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 60,longitude: -95}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: -41,longitude: 174}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 60,longitude: 100}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: -10,longitude: -76 }, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 38.895111,longitude: -77.036667}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 38.895111,longitude: -77.036667}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 60,longitude: 100}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 49,longitude: 32}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 38.895111,longitude: -77.036667}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 38,longitude: -97}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+                {origin: {latitude: 23,longitude: -102}, destination: {
+                latitude: 37.6688,
+                longitude: -122.0808 }, options: {strokeWidth:3 , strokeColor: 'rgba(255, 0, 0, 0.4)', greatArc: true, animationSpeed: 600}},
+
+
+
+
+
+
+
+
+
+
+
+                ]
+
+
+
+
+
+
+
+
+
+
+
 
 
 io.on('connection', function(socket){
   console.log('a user connected');
- socket.emit('message', {'message': fbdestination, for: 'everyone'});
+
+for (i = 0; i < fbdestination.length; i++) {
+
+ socket.emit('message', {'message': fbdestination[i], for: 'everyone'});
+
+}
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
