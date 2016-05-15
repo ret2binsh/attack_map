4897fa7d29279f123d924afd2a6958ffe18734a5
joshftx <joshftx@gmail.com>
2016-05-14 17:49:21 -0700

    consolidated

--- public/web2.js
+++
@@ -1,9 +0,0 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-
-
-var io = require('socket.io')(server);
-app.use(express.static('public'));
-
-io.on('connection', function(){ /* … */ });
-app.listen(3000);
